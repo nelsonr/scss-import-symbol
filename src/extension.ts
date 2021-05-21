71a0bc5f4144f49a1437ecc6f1cb618174851e98
Nelson Rodrigues <nelsonrodrigues87@outlook.com>
2021-05-21 23:32:22 +0100

    First working version

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,35 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+'use strict';
+
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "scss-import-symbol" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('scss-import-symbol.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from scss-import-symbol!');
-	});
-
-	context.subscriptions.push(disposable);
+    context.subscriptions.push(vscode.languages.registerDocumentSymbolProvider(
+        { language: "scss" }, new SCSSImportDocumentSymbolProvider()
+    ));
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {}
+class SCSSImportDocumentSymbolProvider implements vscode.DocumentSymbolProvider {
+    public provideDocumentSymbols(document: vscode.TextDocument, token: vscode.CancellationToken): Thenable<vscode.DocumentSymbol[]> {
+        return new Promise((resolve) => {
+            let symbols: vscode.DocumentSymbol[] = [];
+
+            for (let i = 0; i < document.lineCount; i++) {
+                const line = document.lineAt(i);
+
+                if (line.text.startsWith('@import')) {
+                    let symbol = new vscode.DocumentSymbol(
+                        line.text,
+                        '',
+                        vscode.SymbolKind.File,
+                        line.range,
+                        line.range
+                    );
+
+                    symbols.push(symbol);
+                }
+            }
+
+            resolve(symbols);
+        });
+    }
+}
